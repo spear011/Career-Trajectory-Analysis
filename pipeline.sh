1e44dc3e2d7fdde22a0c373d2f9107315a4a20ac
spear011 <gkspearow@gmail.com>
2025-10-19 16:29:55 +0800

    runs

--- pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=analysis_lightcast
-#SBATCH --partition=foodairesearch 
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=32GB
-#SBATCH --output=/common/home/users/c/chhan/Work/lightcast/runs/%u.%j.out
-
-python main.py
